98b5fb2afe2e36c40294803ca2c328f5e7cd57e6
Spencer Haney <theOfficialSkara@gmail.com>
2018-03-13 01:12:12 -0400

    Initial commit

---
+++ SRex/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SRex
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/lib/jquery-3.2.1.slim.min.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/js/lib/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                    "~/js/lib/popper.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/js/lib/bootstrap.min.js"));
+            
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      //"~/css/bootstrap.min.css",
+                      //"~/css/bootstrap-grid.min.css",
+                      //"~/css/bootstrap-reboot.min.css",
+                      "~/css/bootswatch-lux.min.css",
+                      "~/css/site.css"));
+        }
+    }
+}
---
+++ SRex/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SRex.Startup))]
+namespace SRex
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
