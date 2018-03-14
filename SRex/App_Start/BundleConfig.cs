4e3fb14b047d4fe96400ef0fff443e540b0a9b44
Spencer Haney <theOfficialSkara@gmail.com>
2018-03-14 06:00:02 -0400

    Layout change Home

--- SRex/App_Start/BundleConfig.cs
+++ SRex/App_Start/BundleConfig.cs
@@ -13,8 +13,8 @@ namespace SRex
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/js/lib/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/js/lib/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/popper").Include(
                     "~/js/lib/popper.min.js"));
@@ -23,10 +23,7 @@ namespace SRex
                     "~/js/lib/bootstrap.min.js"));
             
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      //"~/css/bootstrap.min.css",
-                      //"~/css/bootstrap-grid.min.css",
-                      //"~/css/bootstrap-reboot.min.css",
-                      "~/css/bootswatch-lux.css",
+                      "~/css/bootswatch-lux.min.css",
                       "~/css/site.css"));
         }
     }
